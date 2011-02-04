da052c6d47813716f4fae3ed977875d738f2a07e
Alexander Kern <alex@kernul.com>
2011-02-04 13:20:18 -0800

    Done with design

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ BhroboticsCom::Application.routes.draw do
     resources :awards
   end
   
-  match 'about(/:action)',     :controller => :about,     :as => :about
-  match 'team(/:action)',      :controller => :team,      :as => :team
-  match 'media(/:action)',     :controller => :media,     :as => :media
-  match 'community(/:action)', :controller => :community, :as => :community
+  match 'about/:action',     :controller => :about,     :as => :about
+  match 'team/:action',      :controller => :team,      :as => :team
+  match 'media/:action',     :controller => :media,     :as => :media
+  match 'community/:action', :controller => :community, :as => :community
 end
