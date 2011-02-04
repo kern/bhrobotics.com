b8b1496e3716c4214d095f6ab440f35e83973f64
Alexander Kern <alex@kernul.com>
2011-02-04 14:53:57 -0800

    oh my god so much content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     @title = title if title
     @title ? "#{@title} - BHRobotics" : "BHRobotics"
   end
+  
+  def tab_class(tab_title)
+    @title && @title.include?(tab_title) ? raw('class="current"') : ''
+  end
 end
