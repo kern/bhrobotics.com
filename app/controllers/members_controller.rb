68c0bff4ac15cd0d0fdff6b505d896bf70a90847
Alexander Kern <alex@kernul.com>
2011-02-11 15:17:01 -0800

    Remove the awards table.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,5 @@
 class MembersController < ApplicationController
+  def index
+    @members = Member.all
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101215031347) do
-
-  create_table "awards", :force => true do |t|
-    t.string   "name"
-    t.integer  "year"
-    t.string   "regional"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20110211231210) do
 
   create_table "members", :force => true do |t|
     t.string   "name"
