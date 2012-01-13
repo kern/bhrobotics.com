64d8a5b8e7aa5711cb0eb46a64a565b8b5152ad9
Alexander Kern <alex@kernul.com>
2012-01-12 23:07:37 -0800

    Checkpoint.

--- Gemfile
+++ Gemfile
@@ -7,8 +7,9 @@ group :development do
   gem "sqlite3"
 end
 
-group :production do
+group :assets do
   gem "sass-rails", "~> 3.1.5"
   gem "coffee-rails", "~> 3.1.1"
   gem "uglifier", ">= 1.0.3"
+  gem "bourbon"
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  DEFAULT_TITLE = "BHRobotics - FIRST Team 1515 MorTorq"
+  TITLE_SUFFIX = "BHRobotics"
+
+  def title(title)
+    @title = title
+  end
+
+  def render_title
+    if @title.present?
+      "#{@title} - #{TITLE_SUFFIX}"
+    else
+      DEFAULT_TITLE
+    end
+  end
 end
