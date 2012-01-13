6c72141aeb295fed15289c377f74576cc48290f4
Alexander Kern <alex@kernul.com>
2012-01-12 19:42:46 -0800

    Create a new rails app.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "http://rubygems.org"
+
+gem "rails", "3.1.3"
+gem "jquery-rails"
+
+group :development do
+  gem "sqlite3"
+end
+
+group :production do
+  gem "sass-rails", "~> 3.1.5"
+  gem "coffee-rails", "~> 3.1.1"
+  gem "uglifier", ">= 1.0.3"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BHRobotics::Application.config.secret_token = '052ab3e7caa7cd1564996dcac34cfd38e7194d2d9286f7853414d86a3a4a4f13a4faa10b429e84314781009dfbc1bd992075251a32be974acc73b5f6b0205ec7'
