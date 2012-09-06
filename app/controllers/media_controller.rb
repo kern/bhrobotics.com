164b4767edd32d64482d7a271781e49c27d80de3
Mehrzad Far <mehrzadfar@gmail.com>
2012-09-05 20:59:26 -0700

    Changed calendar

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,25 @@
 class MediaController < ApplicationController
+  def photos
+    require 'net/http'
+    require 'rexml/document'
+    picasa_url = "http://picasaweb.google.com/data/feed/api/user/113247014624505710959"
+    picasa_xml = Net::HTTP.get_response(URI.parse(picasa_url))
+    xmol = File.new(picasa_xml.body)
+    doc = REXML::Document.new(xmol)
+    @picasa_doc = doc
+    #doc.elements.each("gphoto:id/gphoto:name") { |element| puts element }
+
+
+    #@picasa_albums = []
+    #for album in picasa_doc.elements.to_a
+    #	@picasa_albums << post.elements['gphoto'].text
+    #end
+
+   # url = URI.parse('http://picasaweb.google.com/data/feed/api/user/113247014624505710959')
+   # req = Net::HTTP::Get.new(url.path)
+   # res = Net::HTTP.start(url.host, url.port) {|http|
+   #   http.request(req)
+   # }
+   # @albums = res.body
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,3 +2,6 @@ class Member < ActiveRecord::Base
   validates_presence_of :name
   validates_inclusion_of :category, :in => %w{student mentor alumni}
 end
+
+# <% require 'net/http' %>
+# <% Net::HTTP.get(URI.parse('https://picasaweb.google.com/data/feed/api/user/113247014624505710959')) %>
