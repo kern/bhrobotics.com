5ee83953a3169157bf49a85d989449775bf1ca5e
Mehrzad Far <mehrzadfar@gmail.com>
2013-01-09 16:41:22 -0800

    Photos stuff

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "http://rubygems.org"
 
 gem "rails", "3.1.3"
 gem "jquery-rails"
-
+gem "nokogiri"
 group :development do
   gem "sqlite3"
 end
--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,12 +1,13 @@
 class MediaController < ApplicationController
- def photos
-    require 'net/http'
-    require 'rexml/document'
-    picasa_url = "http://picasaweb.google.com/data/feed/api/user/113247014624505710959"
-    picasa_xml = Net::HTTP.get_response(URI.parse(picasa_url))
-    xmol = File.new(picasa_xml.body)
-    doc = REXML::Document.new(xmol)
-    @picasa_doc = doc
+# =begin
+ #def photos
+  #  require 'net/http'
+   # require 'rexml/document'
+    #picasa_url = "http://picasaweb.google.com/data/feed/api/user/113247014624505710959"
+    #picasa_xml = Net::HTTP.get_response(URI.parse(picasa_url))
+    #xmol = File.new(picasa_xml.body)
+    #doc = REXML::Document.new(xmol)
+    #@picasa_doc = doc
     #doc.elements.each("gphoto:id/gphoto:name") { |element| puts element }
 
 
@@ -21,5 +22,34 @@ class MediaController < ApplicationController
    #   http.request(req)
    # }
    # @albums = res.body
+ # end
+  
+#  def photos
+    #require 'nokogiri'
+    #require 'open-uri'  
+    #require 'rubygems'
+   # content = File.read('input.xml')
+  #  doc = Nokogiri::XML(content) {|config| 
+           #   config.options = Nokogiri::XML::ParseOptions::STRICT
+          #}
+          
+    #hashes = doc.xpath('//xmlns:entry').map do |entry|
+      #{
+        #:id => entry.xpath('gphoto:id').inner_text,
+       # :thumb => entry.parent.xpath('gphoto:thumbnail').inner_text,
+      #  :name => entry.xpath('gphoto:name').inner_text,
+     #   :count => entry.xpath('gphoto:numphotos').inner_text
+    #  }
+   # end
+    #
+  #  puts hashes.inspect
+    
+ # end
+  
+  def photos
+    require 'nokogiri'
+    require 'open-uri'
+    doc = Nokogiri::HTML(open('http://picasaweb.google.com/data/feed/api/user/113247014624505710959'))
+    @picasa_doc = doc
   end
 end
