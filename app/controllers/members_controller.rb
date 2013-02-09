ce996339f3359560067f58c087fec9a9e1ddb422
Mehrzad Far <mehrzadfar@gmail.com>
2013-02-09 11:46:19 -0800

    Breaking members for a moment

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,8 +4,11 @@ class MembersController < ApplicationController
   def index
     #:category => "student" && 
    # @member = Member.find#(params[:id]) <- no sense in this context
-    @students = Member.where("graduation_year >= ?", Time.now.year).order("name ASC") 
-    @alumni = Member.where("graduation_year < ?", Time.now.year).order("name ASC")   
+   # @students = Member.where("graduation_year >= ?", Time.now.year).order("name ASC") 
+   # @alumni = Member.where("graduation_year < ?", Time.now.year).order("name ASC")   
+    @students = Member.where(:graduation_year.to_s.to_i >= Time.now.year).order("name ASC")
+    @alumni = Member.where(:graduation_year.to_s.to_i < Time.now.year).order("name ASC")
+
     @mentors = Member.where(:category => "mentor").order("name ASC")    
   end
 
