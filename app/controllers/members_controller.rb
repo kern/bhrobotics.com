8c722e881001d2041e260a59c9c108a78fbf6812
Mehrzad Far <mehrzadfar@gmail.com>
2013-02-11 12:51:06 -0800

    graduation_year fix

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,11 +4,10 @@ class MembersController < ApplicationController
   def index
     #:category => "student" && 
    # @member = Member.find#(params[:id]) <- no sense in this context
-   # @students = Member.where("graduation_year >= ?", Time.now.year).order("name ASC") 
-   # @alumni = Member.where("graduation_year < ?", Time.now.year).order("name ASC")   
-    @students = Member.where(:category => "student").order("name ASC")
-    @alumni = Member.where(:category => "alumni").order("name ASC")
-
+  #@students = Member.where(:category => "student").order("name ASC")
+    #@alumni = Member.where(:category => "alumni").order("name ASC")
+    @students = Member.where("graduation_year >= ?", Time.now.year).order("name ASC") 
+    @alumni = Member.where("graduation_year < ?", Time.now.year).order("name ASC")    
     @mentors = Member.where(:category => "mentor").order("name ASC")    
   end
 
